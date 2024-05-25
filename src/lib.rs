99009f610313df786d776306986b2b8fb48f3a6b
Benjamin Massey <benjamin.w.massey@gmail.com>
2024-05-24 17:00:41 -0700

    Attempt at utilizing llama_cpp_rs crate instead of llm. Takes GGUF files now, which is a bonus for modernity. It also loads via CUDA, as wanted, but is not getting the performance it should be getting, considering this fact. The ton of console output on model load is also annoying to not be able to be disabled.

--- src/lib.rs
+++ src/lib.rs
@@ -1,81 +1,43 @@
-use llm::{InferenceParameters, Model};
-
-const USE_GPU: bool = true;
-
-pub struct LLM {
-    model: llm::models::Llama,
-    session: llm::InferenceSession,
-}
+use llama_cpp_rs::{
+    options::{ModelOptions, PredictOptions},
+    LLama,
+};
 
 fn contexted_prompt(query: &str) -> String {
     format!(
-r#"### System:
+r#"# System:
 You are an AI assistant who gives a quality response to whatever humans ask of you.
 
-### Human:
+# Human:
 {query}
 
-### Assistant:
+# Assistant:
 "#)
 }
 
-fn last_n_chars(string: &str, n: usize) -> String {
-    if string.len() <= n {
-        return string.to_owned();
-    }
-    string[
-        string
-            .char_indices()
-            .nth_back(n - 1)
-            .unwrap().0..
-    ].to_owned()
+pub fn init(llama_path: &str) -> LLama {
+    let model_options = ModelOptions {
+        n_gpu_layers: 30,
+        ..Default::default()
+    };
+    LLama::new(llama_path.into(), &model_options).unwrap()
 }
 
-pub fn init(llama_path: &str) -> LLM {
-    let mut params: llm::ModelParameters = Default::default();
-    params.use_gpu = USE_GPU;
-    let model = llm::load::<llm::models::Llama>(
-        std::path::Path::new(llama_path),
-        llm::TokenizerSource::Embedded,
-        params,
-        |_|{},
-    )
-    .unwrap_or_else(|err| panic!("Failed to load model: {err}"));
-    let session = model.start_session(Default::default());
-    LLM { model, session }
-}
-
-pub fn chat(llm: &mut LLM, prompt: &str, tokens: Option<usize>) -> String {
+pub fn chat(llama: &mut LLama, prompt: &str, tokens: Option<usize>) -> String {
     let prompt = &contexted_prompt(prompt);
-    let mut response = String::new();
-    let _ = llm.session.infer::<std::convert::Infallible>(
-        &llm.model,
-        &mut rand::thread_rng(),
-        &llm::InferenceRequest {
-            prompt: llm::Prompt::Text(prompt),
-            parameters: &InferenceParameters::default(),
-            play_back_previous_tokens: false,
-            maximum_token_count: tokens,
-        },
-        &mut Default::default(),
-        |t| { 
-            match t {
-                llm::InferenceResponse::InferredToken(x) => {
-                    if last_n_chars(&response, 3) == "###" {
-                        return Ok(llm::InferenceFeedback::Halt);
-                    }
-                    response = response.clone() + &x;
-                },
-                llm::InferenceResponse::EotToken => {
-                    return Ok(llm::InferenceFeedback::Halt);
-                },
-                _ => {},
-            };
-
-            Ok(llm::InferenceFeedback::Continue)
-        }
-    );
-    response
+    let predict_options = PredictOptions {
+        tokens: if tokens.is_none() { 0 } else { tokens.unwrap() as i32 },
+        token_callback: Some(Box::new(|token| {
+            token != "#"
+        })),
+        ..Default::default()
+    };
+    llama
+        .predict(
+            prompt.into(),
+            predict_options,
+        )
+        .unwrap()
 }
 
 pub fn lazy_chat(llama_path: &str, prompt: &str) -> String {
